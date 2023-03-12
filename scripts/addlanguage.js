9bed8472cd3d5fd32841c8652721fd359aeddc74
Ivan Cheng <ivan.cheng011099@gmail.com>
2023-03-11 19:19:45 -0800

    added cancel button

--- scripts/addlanguage.js
+++ scripts/addlanguage.js
@@ -1,13 +1,17 @@
 
-$(document).ready(
-    $("#addlanguage").click(function () {
+$(document).ready(function() {
+    $("#addLanguage").click(function () {
         $(".more-languages").append(
             `
             <div class="language-group">
-                <input type="text">
+                <input type="text" id="newLanguage">
                 <input type="range" class="range" min="0" max="100" class="slider">
             </div>
             `
         );
-    })
-);
+    });
+
+    $("#cancelLanguage").click(function () {
+        $(".more-languages").remove();
+    });
+});
---
+++ scripts/button.js
@@ -0,0 +1,28 @@
+console.log("fuck this")
+var addLanguageButton;
+var cancelButton;
+
+function addLanguage() {
+    addLanguageButton = document.getElementById("addLanguage");
+    cancelButton = document.getElementById("cancelLanguage");
+    addLanguageButton.style.display ="none";
+    cancelButton.style.display="block";
+    cancelButton.onclick = cancelLanguage;
+
+}
+
+function cancelLanguage() {
+    console.log("this cancelled");
+    addLanguageButton = document.getElementById("addLanguage");
+    cancelButton = document.getElementById("cancelLanguage");
+    cancelButton.style.display="none";
+    addLanguageButton.style.display ="block";
+    addLanguageButton.onclick = addLanguage;
+    
+}
+
+var addLanguageButton = document.getElementById("addLanguage");
+
+
+addLanguageButton.onclick = addLanguage;
+cancelButton.onclick = cancelLanguage;
