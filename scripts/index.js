e27dbac5d2c3bbc07d3d484e44b8fc085f253645
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-03-12 00:27:18 -0800

    Add bubbles

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
+  const audio = new Audio("/audio/bubbles.mp3");
     $("#banner-button").click(function() {
+      $('#bubbles-audio')[0].play();
       window.location.href = "/bubbles.html";
     });
   });
   
+
