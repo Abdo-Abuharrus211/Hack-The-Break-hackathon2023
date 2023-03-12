a1782838acc43d5ab938a5ff92362286212f82e5
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-03-11 16:46:27 -0800

    make static

---
+++ scripts/skeleton.js
@@ -0,0 +1,5 @@
+function loadSkeleton() {
+    console.log($('#my-navbar').load('./partials/navbar.html'));
+    console.log($('#my-footer').load('./partials/footer.html'));
+}
+loadSkeleton();  //invoke the function
