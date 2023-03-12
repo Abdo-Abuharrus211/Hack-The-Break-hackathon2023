9d373bfe26eb03236f1c366638b476f3b194623f
I-wen Su <grace2268@hotmail.com>
2023-03-11 16:34:08 -0800

    bubble debug

--- scripts/bubbles.js
+++ scripts/bubbles.js
@@ -1,49 +1,54 @@
-function bubblify(){
+function bubblify() {
 
     var nodes = new vis.DataSet([
-        {label: "Pop"},
-        {label: "Alternative"},
-        {label: "Rock"},
-        {label: "Jazz"},
-        {label: "Hits"},
-        {label: "Dance"},
-        {label: "Metal"},
-        {label: "Experimental"},
-        {label: "Rap"},
-        {label: "Electronic"},
+        { label: "Pop" },
+        { label: "Alternative" },
+        { label: "Rock" },
+        { label: "Jazz" },
+        { label: "Hits" },
+        { label: "Dance" },
+        { label: "Metal" },
+        { label: "Experimental" },
+        { label: "Rap" },
+        { label: "Electronic" },
     ]);
     var edges = new vis.DataSet();
-    
+
     //   var container = document.getElementById('bubbles');
     var container = $("#bubbles")
     var data = {
         nodes: nodes,
         edges: edges
     };
-    
+
     var options = {
-        nodes: {borderWidth:0,shape:"circle",color:{background:'#F92C55', highlight:{background:'#F92C55', border: '#F92C55'}},font:{color:'#fff'}},
+        nodes: { borderWidth: 0, shape: "circle", color: { background: '#F92C55', highlight: { background: '#F92C55', border: '#F92C55' } }, font: { color: '#fff' } },
         physics: {
             stabilization: false,
-            minVelocity:  0.01,
+            minVelocity: 0.01,
             solver: "repulsion",
             repulsion: {
                 nodeDistance: 40
             }
-    }
-};
-var network = new vis.Network(container, data, options);
+        }
+    };
+    var network = new vis.Network(container, data, options);
 
 
-// Events
-network.on("click", function(e) {
-    if (e.nodes.length) {
-      var node = nodes.get(e.nodes[0]);
-      // Do something
-      nodes.update(node);
+    // Events
+    network.on("click", function (e) {
+        if (e.nodes.length) {
+            var node = nodes.get(e.nodes[0]);
+            // Do something
+            nodes.update(node);
+        }
     }
-}
-  );
+    );
+
+    container.on("mouse-wheel", function (event) {
+        // prevents zooming with the mouse-wheel event
+        event.stopPropagation();
+    });
 }
 
 bubblify()
